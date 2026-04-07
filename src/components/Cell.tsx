ca8361889b2ae9c5f04773fe70484a22223a64dc
samantha <samanthap8675@gmail.com>
2026-04-07 15:21:05 -0400

    fixed grid color

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const CellDiv = styled.div`
     width: 60px;
     height: 60px;
-    border: 2px solid white;
+    border: 2px solid gray;
 `;
 export default function Cell() {
     return <CellDiv></CellDiv>
