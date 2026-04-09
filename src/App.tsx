02f7400be153312d033fd3c98fe93aff86632791
Callie Liu <callie26@bu.edu>
2026-04-09 00:51:40 -0400

    created a guessColorsProp and logic to check what key colors needed to be changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+// File: final-project/src/App.tsx
+// Author: Tiffany Yam (tiffyam@bu.edu), Callie Liu (callie26@bu.edu) 4/5/2026
+// Description: The file used to define the logic of the wordle game.
+
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 
@@ -5,6 +9,7 @@ import Grid from './components/Grid'
 import Keyboard from "./components/Keyboard.tsx";
 import GameResult from "./components/GameResult";
 import NavBar from "./components/Nav";
+import type {GuessColorsProps} from "./types/GuessColorsProps.ts";
 
 const MainDiv = styled.div`
   display: flex;
@@ -30,6 +35,7 @@ function App() {
     // states for keyboard color change
     const [currentGuess, setCurrentGuess] = useState("");
     const [answer, setAnswer] = useState("");
+    const [keyboardColors, setKeyboardColors] = useState<GuessColorsProps[]>([]);
 
     // fetch a random 5-letter word from the API
     useEffect(()=>{
@@ -44,8 +50,8 @@ function App() {
     }, []);
 
     // check a guess against the answer (need to put this in when enter is hit i think)
-    function getGuessColors(guess:string, answer:string){
-        const guessColors = [];
+    function getGuessColors(guess:string, answer:string):GuessColorsProps[]{
+        const guessColors:GuessColorsProps[] = [];
         // make string into array for easy manipulation
         // https://www.geeksforgeeks.org/javascript/string-to-array-in-javascript/
         const remainingLetters = answer.split("");
@@ -57,10 +63,11 @@ function App() {
                 remainingLetters[i] = "*"
             }
 
-        // check for letters not in correct spot but in word
-        // https://www.w3schools.com/jsref/jsref_includes_array.asp
+            // check for letters not in correct spot but in word
+            // https://www.w3schools.com/jsref/jsref_includes_array.asp
             else if (remainingLetters.includes(guess[i])) {
                 guessColors[i] = {letter: guess[i], color: "yellow"};
+                // https://www.w3schools.com/jsref/jsref_indexof_array.asp
                 const indexOfCorrectSpot = remainingLetters.indexOf(guess[i]);
                 remainingLetters[indexOfCorrectSpot] = "*"
             }
@@ -69,8 +76,35 @@ function App() {
         return guessColors;
     }
 
-    // still need to check whether to update the keyboard key colors
+    // check whether to update the keyboard key colors
+    function updateKeyboardColors(guessColors:GuessColorsProps[]):GuessColorsProps[]{
+        // get current keyboard colors and update or add colors if needed
+        const updatedKeyboardColors:GuessColorsProps[] = [...keyboardColors];
 
+        // loop through each guess color
+        // https://www.w3schools.com/jsref/jsref_foreach.asp
+        guessColors.forEach((guessColor: GuessColorsProps)=> {
+
+            // see if there is already existing color for the guess letter
+            // https://stackoverflow.com/questions/67476440/how-to-match-key-value-in-array-of-objects
+            const existingColor = updatedKeyboardColors.find(updatedKeyboardColor => updatedKeyboardColor.letter === guessColor.letter)
+
+            // if there is already existing color then only update if guess color has higher precedence
+            if (existingColor) {
+                if ((guessColor.color === "green") || (existingColor.color !== "green" && guessColor.color === "yellow")) {
+                    existingColor.color = guessColor.color
+                }
+            }
+            // else it is the first time adding a color to this letter so add it to the array
+            else {
+                if (guessColor.color === "green" || guessColor.color === "yellow") {
+                    // https://www.w3schools.com/jsref/jsref_push.asp
+                    updatedKeyboardColors.push(guessColor);
+                }
+            }
+        });
+        setKeyboardColors(updatedKeyboardColors)
+    }
 
 
 
---
+++ src/types/GuessColorsProps.ts
@@ -0,0 +1,8 @@
+// File: final-project/src/types/GuessColorProps.tsx
+// Author: Callie Liu (callie26@bu.edu), 4/8/2026
+// Description: The file used to contain types of guess colors for the Keyboard component.
+
+export type GuessColorsProps = {
+    letter: string;
+    color: "green" | "yellow" | "gray";
+}
