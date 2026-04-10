f43e1d12ae9d9b652e512b3388db6fe3b6459355
Callie Liu <callie26@bu.edu>
2026-04-10 14:29:10 -0400

    added the colors gotten from the functions to keyboard by passing down as props

--- src/components/KeyButton.tsx
+++ src/components/KeyButton.tsx
@@ -6,8 +6,12 @@ import styled from "styled-components";
 
 // used to style each key button of the keyboard if not ENTER key or BACKSPACE
 // https://stackoverflow.com/questions/68683137/how-to-style-multiple-variations-of-a-button-with-styled-components
-const StyledButton = styled.button`
-    background-color: #5c5c5c;
+const StyledButton = styled.button<{color:string | undefined}>`
+    background-color: ${(props)=> {
+        if (props.color === "green") return "#A8DCAB"
+        else if (props.color === "yellow") return "#FFEE8C"
+        else return "#5C5C5C"
+    }};
     color: white;
     width: 45px;
     height: 55px;
@@ -23,8 +27,12 @@ const StyledButton = styled.button`
 `;
 
 // used to style each key button of the keyboard when ENTER key or BACKSPACE
-const StyledWideButton = styled.button`
-    background-color: #5c5c5c;
+const StyledWideButton = styled.button<{color:string | undefined}>`
+    background-color: ${(props)=> {
+        if (props.color === "green") return "#A8DCAB"
+        else if (props.color === "yellow") return "#FFEE8C"
+        else return "#5C5C5C"
+    }};
     color: white;
     width: 100px;
     height: 55px;
@@ -39,13 +47,13 @@ const StyledWideButton = styled.button`
     }
 `;
 
-export default function KeyButton(props: {letter: string}) {
+export default function KeyButton(props: {letter: string, color: string | undefined}) {
     return (
         // if the letter of the key is ENTER or BACKSPACE
         props.letter === "ENTER" || props.letter === "BACKSPACE" ?
             // display button as a wide button
-            <StyledWideButton> {props.letter} </StyledWideButton>:
+            <StyledWideButton color={props.color}> {props.letter} </StyledWideButton>:
             // else display as regular button
-            <StyledButton> {props.letter} </StyledButton>
+            <StyledButton color={props.color}> {props.letter} </StyledButton>
     );
 }
